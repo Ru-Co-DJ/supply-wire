54685e6172c2d85af949130dcd3d4fa08ff3c475
Ru-Co-DJ <devdzcas@gmail.com>
2022-11-02 09:09:04 +0100

    Basic react routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Auth from "./pages/Auth/Auth";
+import BestSales from "./pages/BestSales/BestSales";
+import CustomerService from "./pages/CustomerService/CustomerService";
+import Categories from "./pages/Categories/Categories";
+import Products from "./pages/Products/Products";
+import Missing from "./pages/Missing/Missing";
+import Account from "./pages/Account/Account";
+
 function App() {
   return (
-    <div>
-     
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/account" element={<Account/>}/>
+          <Route path="/auth" element={<Auth/>}/>
+          <Route path="/best-sales" element={<BestSales/>}/>
+          <Route path="/customer-service" element={<CustomerService/>}/>
+          <Route path="/categories" element={<Categories/>}/>
+          <Route path="*" element={<Missing/>}/>
+          <Route path="/products" element={<Products/>}/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
   );
 }
 
